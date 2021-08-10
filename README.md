1a342ee74ef9807c20103630b6fded73e1f9d680
irismtz <51760990+irismtz@users.noreply.github.com>
2021-08-10 05:56:54 -0500

    Estructura de la página

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# inventario_usuarios
-Página web de un inventario de usuarios.
+# CRUD sencillo en PHP y MySQL
+<img src="CRUD%20sencillo%20en%20PHP%20y%20MySQL.png">
+Se comparte el código libremente y para descargar la base de datos puedes acceder al siguiente enlace:
+https://www.configuroweb.com/46-aplicaciones-gratuitas-en-php-python-y-javascript/#crud-sencillo
---
+++ home.php
@@ -0,0 +1,35 @@
+<?php
+	include("session.php");
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/mystyle.css" /> 
+</head>
+
+<body>
+<div class="icon-bar">
+  <a class="active" href="home.php"><i class="fa fa-home"></i></a> 
+  <a href="users.php"><i class="fa fa-user"></i></a> 
+  <a href="registration.php"><i class="fa fa-registered"></i></a>
+  <a href="logout.php"><i class="fa fa-power-off"></i></a> 
+</div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+<div class="container">
+  <div>
+  <center>
+  <font size="+5">Inventario de los usuarios <3  :) </font>
+  <img src="images/bts.jpg" aling="center" width="1200" height="600">
+  <br> <font size="+2">I love BTS <3 </font></br>
+  </center>
+  </div>
+</div>
+
+</body>
+</html> 
